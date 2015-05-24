0c1caf2b838ee4b71db0a98069c62451682ecf5a
Nico <itsarabbit@gmail.com>
2015-05-24 21:15:51 +0200

    Added shitty goal thing particle

--- Assets/Scripts/GlobalVars.cs
+++ Assets/Scripts/GlobalVars.cs
@@ -4,19 +4,12 @@ using System.Timers;
 using UnityEngine;
 using System.Collections;
 
-public class GlobalVars : MonoBehaviour
+public static class GlobalVars
 {
-    public static float WorldRadius = 10;
-    private float time = 0;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-        //Varies the world radius
-	    //WorldRadius += Time.deltaTime*Mathf.Sin(Time.time * 20) * 9;
-	}
+    public static float WorldRadius
+    {
+        get { return Level + 20; }
+    }
+    private static float time = 0;
+    public static int Level = 1;
 }
